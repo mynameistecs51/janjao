8b8085637e812de94b3b7ecd592f6d3896a818be
chaiwat <mynameistecs51@gmail.com>
2017-08-14 22:54:53 +0700

    update logo

--- application/views/checkin/Bill.php
+++ application/views/checkin/Bill.php
@@ -25,7 +25,9 @@
 						</tr>
 						<tr>
 							<td colspan="2"  style="widows: 100%;text-align: left;">
-								<div class="headname"><img src="<?php echo base_url().'assets/images/logoBlack.png'; ?>" width="150"/></div>
+								<div class="headname">
+									<img src="<?php echo base_url().'assets/images/logoBlack.png'; ?>" width="150" />
+								</div>
 								<!-- จันทร์เจ้า โฮเทล  <br>-->
 								<!-- 666 หมู่ 6	ถ.โพนพิสัย ต.หมากแข้ง 	อ.เมือง จ.อุดรธานี 41000 <br>
 								โทร 0910571616 <br>
